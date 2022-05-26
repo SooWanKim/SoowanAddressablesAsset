fa2e30da03e8c693a5e74aed81ed5fed45a20fdd
SoowanKim <herosdoor@paran.com>
2022-05-26 18:32:34 +0900

    add asset add basicreferences add listofreferences
    add asset
    add basicreferences
    add listofreferences

---
+++ Assets/Scripts/BasicReference.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+
+public class BasicReference : MonoBehaviour
+{
+	public AssetReference baseCube;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SpawnThing()
+	{
+		baseCube.InstantiateAsync();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SoowanAddressablesAsset
+# Soowan Addressables Asset
 Build AssetBundle, Use Addressables Asset
+
+## Basic Reference
+AssetReference을 사용하여 큐브 생성
+
+## ListOfReferences
+AssetReference을 사용하여 비동기로딩이 끝나면 애셋 생성
+
+## 번들 빌드
